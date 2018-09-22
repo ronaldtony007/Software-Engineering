7ed00bcceb014a3597b5a4452bd5ed91277587e9
ronaldtony007 <ronaldtony007@gmail.com>
2018-09-22 00:24:28 -0700

    Formatted index.php

--- CSS/index.php
+++ CSS/index.php
@@ -1,15 +1,15 @@
 <?php 
-  session_start(); 
+    session_start(); 
 
-  if (!isset($_SESSION['username'])) {
-  	$_SESSION['msg'] = "You must log in first";
-  	header('location: login.php');
-  }
-  if (isset($_GET['logout'])) {
-  	session_destroy();
-  	unset($_SESSION['username']);
-  	header("location: login.php");
-  }
+    if (!isset($_SESSION['username'])) {
+        $_SESSION['msg'] = "You must log in first";
+        header('Location: login.php');
+    }
+    if (isset($_GET['logout'])) {
+        session_destroy();
+        unset($_SESSION['username']);
+        header("Location: login.php");
+    }
 ?>
 <!DOCTYPE html>
 <html>
