662299f31723e3da412705c59eb495b28bdde4b9
ronaldtony007 <ronaldtony007@gmail.com>
2018-11-06 19:44:23 +0530

    Made authentication for admin side

--- soft/Admin/authenticate.php
+++ soft/Admin/authenticate.php
@@ -14,15 +14,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$pass = $_POST['pass'];
 	$pass = filter_var($pass, FILTER_SANITIZE_STRING);
 	$pass = stripslashes($pass);
-	$salt = "$6$rounds=5000$NIC$";
-	$pass = mysqli_real_escape_string($conn, crypt($pass, $salt));
+	$pass = mysqli_real_escape_string($conn, $pass);
 
-	$sql = "SELECT * FROM registration WHERE username = '".$user."' and password = '".$pass."'";
+	$hash = '$2y$10$yl9RsK86L2g16NnLhtmRpeQxxPbqiueG0GYBoX/HpVcESq0StEZdW';
 
-	$result = mysqli_query($conn, $sql);
-	$count = mysqli_num_rows($result);
-
-	if ($count === 1) {
+	if ($user === "admin" && password_verify($pass, $hash)) {
 		$_SESSION['user'] = $user;
 		closeConnection($conn);
 		header("Location: dashboard.php", true, 301);
@@ -30,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	}
 	else {
 		closeConnection($conn);
-		echo "<script language = \"javascript\">alert('Invalid Credentials')</script>";
+		echo "<script language = \"javascript\">alert('Invalid Credentials $pass $hash')</script>";
 		echo "<script>setTimeout(\"location.href = 'login.html'\",20);</script>";
 	}
 
--- soft/Trial.php
+++ soft/Trial.php
@@ -1,60 +1,10 @@
 <?php
-// 2 character salt
-if (CRYPT_STD_DES == 1)
-{
-echo "Standard DES: ".crypt('something','st')."\n<br>"; 
-}
-else
-{
-echo "Standard DES not supported.\n<br>";
-}
-
-// 4 character salt
-if (CRYPT_EXT_DES == 1)
-{
-echo "Extended DES: ".crypt('something','_S4..some')."\n<br>";
-}
-else
-{
-echo "Extended DES not supported.\n<br>";
-}
-
-// 12 character salt starting with $1$ 
-if (CRYPT_MD5 == 1)
-{
-echo "MD5: ".crypt('something','$1$somethin$')."\n<br>"; 
-}
-else
-{
-echo "MD5 not supported.\n<br>";
-}
-
-// Salt starting with $2a$. The two digit cost parameter: 09. 22 characters 
-if (CRYPT_BLOWFISH == 1)
-{
-echo "Blowfish: ".crypt('something','$2a$09$anexamplestringforsalt$')."\n<br>"; 
-}
-else
-{
-echo "Blowfish DES not supported.\n<br>";
-}
-
-// 16 character salt starting with $5$. The default number of rounds is 5000.
-if (CRYPT_SHA256 == 1) 
-{
-echo "SHA-256: ".crypt('something','$5$rounds=5000$anexamplestringforsalt$')."\n<br>"; }
-else
-{
-echo "SHA-256 not supported.\n<br>";
-}
-
-// 16 character salt starting with $6$. The default number of rounds is 5000.
-if (CRYPT_SHA512 == 1) 
-{
-echo "SHA-512: ".crypt('pass','$6$rounds=5000$anexamplestringforsalt$'); 
-}
-else
-{
-echo "SHA-512 not supported.";
-}
+/**
+ * We just want to hash our password using the current DEFAULT algorithm.
+ * This is presently BCRYPT, and will produce a 60 character result.
+ *
+ * Beware that DEFAULT may change over time, so you would want to prepare
+ * By allowing your storage to expand past 60 characters (255 would be good)
+ */
+echo password_hash("Administrator", PASSWORD_DEFAULT);
 ?>
